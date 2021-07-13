9a04b239366cfef286894248cf01acc1c051b661
DumToryo <83193243+thiendum2020@users.noreply.github.com>
2021-07-13 08:35:30 +0700

    z

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -11,15 +11,15 @@ class LoginController extends Controller
 {
     public function login(Request $request) {
         $user = UserModel::where(['username' => $request->get('username')])->first();
-        
+
         if($user == null)
-            return response()->json(['status' => 1, 'message' => 'Tài khoản chưa được đăng ký!']);
+            return response()->json(['status' => 0, 'msg' => 'Username does not exist','data'=>null], 404);
 
         else {
             if($user->password != $request->get('password'))
-                return response()->json(['status' => 2, 'message' => 'Sai mật khẩu!']);
+                return response()->json(['status' => 2, 'msg' => 'Password is incorrect','data'=>null], 400);
             else
-                return response()->json(['status' => 0, 'data' => $user]);
+                return response()->json(['status' => 0, 'msg'=>'success', 'data' => $user]);
         }
     }
 }
